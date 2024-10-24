c05682441a8de4cf60756da1eecaa104fc557f73
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-24 09:01:09 -0700

    Adds MovieCard component to app.jsx and renders it with real data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import MovieCard from "./components/MovieCard/MovieCard";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     const API_KEY = import.meta.env.VITE_TMDBKEY;
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        setMovies(data?.results);
+        setLoading(false);
       });
   }, []);
-  return <></>;
+  return (
+    <>
+      <div className="movies">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
