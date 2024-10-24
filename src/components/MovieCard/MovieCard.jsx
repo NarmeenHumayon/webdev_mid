9eab11161d4c471e9e9a5755859e134b9815e1f4
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-24 09:13:25 -0700

    Populates MovieCard component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,4 +1,18 @@
-const MovieCard = (props) => {
-  return <h1 {...props}>Movie Card</h1>;
+import "./MovieCard.css";
+const MovieCard = ({ movie, ...props }) => {
+  return (
+    <div className="card">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+        alt={movie.original_title}
+        className="backdrop"
+      />
+      <div className="flex-hor">
+        <div className="title">{movie.original_title}</div>
+        <div className="rating">{movie.vote_average}/10</div>
+      </div>
+      <p className="desc truncate-overflow">{movie.overview}</p>
+    </div>
+  );
 };
 export default MovieCard;
