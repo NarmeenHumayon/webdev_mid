2a22134d3bc6c781d5c3b79fba4740bd0224855b
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-24 10:52:02 -0700

    Adds favourite state to context

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,29 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
+  const [favourite, setFavourites] = useState(false);
   useEffect(() => {
     setLoading(true);
     const API_KEY = import.meta.env.VITE_TMDBKEY;
     let url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1`;
     if (query) {
       url = ` https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&page=1`;
+    }
+    if (favourite) {
+      const existingIds = JSON.parse(localStorage.getItem("ids")) || [];
+      setMovies([]);
+      existingIds.map((id) => {
+        axios
+          .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
+          .then((res) => {
+            setMovies((prevMovies) => [...prevMovies, res.data]);
+          })
+          .catch((e) => {
+            setError(e);
+          });
+      });
+      setLoading(false);
+      return;
     }
     axios
       .get(url)
@@ -28,10 +45,12 @@ function App() {
       .catch((e) => {
         setError(e);
       });
-  }, [query]);
+  }, [query, favourite]);
   return (
     <>
-      <queryContext.Provider value={{ query, setQuery }}>
+      <queryContext.Provider
+        value={{ query, setQuery, favourite, setFavourites }}
+      >
         <Navbar />
         <div className="movies">
           {loading ? (
