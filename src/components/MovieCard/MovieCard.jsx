4adfc38a5303c2f2aa885e3d26eef1202a880770
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-24 09:36:07 -0700

    Adds margin to movie card title

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -7,7 +7,14 @@ const MovieCard = ({ movie, ...props }) => {
         alt={movie.original_title}
         className="backdrop"
       />
-      <div className="flex-hor">
+      <div
+        className="flex-hor"
+        style={{
+          margin: "5px",
+          marginTop: "0",
+          marginBottom: "10px",
+        }}
+      >
         <div className="title">{movie.original_title}</div>
         <div className="rating">{movie.vote_average}/10</div>
       </div>
