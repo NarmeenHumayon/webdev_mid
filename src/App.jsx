010382f95c56e0da97ecd28b2795aa58418f83c7
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-24 09:05:25 -0700

    Hits api using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./components/MovieCard/MovieCard";
+import axios from "axios";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   useEffect(() => {
     setLoading(true);
     const API_KEY = import.meta.env.VITE_TMDBKEY;
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMovies(data?.results);
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+        setMovies(res?.data?.results);
         setLoading(false);
+      })
+      .catch((e) => {
+        setError(e);
       });
   }, []);
   return (
